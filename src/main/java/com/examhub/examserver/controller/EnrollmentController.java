3034c9600b9f89f7fb7ddc134d3c48abb05f1fe8
Srijita Dasgupta <srijitadasgupta11@gmail.com>
2026-02-21 19:51:56 +0530

    fixed a leading slash that i missed previously in the enrollment controller

--- src/main/java/com/examhub/examserver/controller/EnrollmentController.java
+++ src/main/java/com/examhub/examserver/controller/EnrollmentController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/enrollments")
+@RequestMapping("/api/v1/enrollments")
 @RequiredArgsConstructor
 public class EnrollmentController {
 
